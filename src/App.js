1d0f2d314e8447792f299db214525c8b8e52359d
dela-hashem <dela.hashem89@gmail.com>
2025-06-27 16:22:44 +0800

    trigger real deploy test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NewsList from './components/NewsList';
 
 function App() {
   return (
+    // مثلا تو App.js
+    console.log("deploy test");
     <div className="App">
       <NewsList />
     </div>
